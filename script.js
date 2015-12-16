87cf566682768162fbd7f3b35f0ebecd1f96f5a6
christopherrosario <elpapi26_@hotmail.com>
2015-12-16 22:57:27 +0000

    pizza-address

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {
     $( "#enter" ).click( function(e) {
-        var  client_id ="0XOI0ZS0KEER4HN4TX2RR5OZP4APUJVPFN01TCM41V5BHDZX"
-    var client_secret  ="VTCMVBG4NZBQT0EK4EUOOBFPDHC20XBZ5D1G1R1QYWWR4JVU"
+        var  client_id ="0XOI0ZS0KEER4HN4TX2RR5OZP4APUJVPFN01TCM41V5BHDZX";
+    var client_secret  ="VTCMVBG4NZBQT0EK4EUOOBFPDHC20XBZ5D1G1R1QYWWR4JVU";
           $.get("https://api.foursquare.com/v2/venues/search" +
         "?client_id=" + client_id +
         "&client_secret=" + client_secret +
@@ -19,9 +19,12 @@ $( document ).ready(function() {
         "&v=20151209" +
         "&near=nyc" +
         "&query=pizza",
-        function(data) {
-            console.log(data);
+        function(data) { 
+            
+            
+            console.log(data["response"]["venues"][0]["name"]["contact"]["phone"]);
             // Callback code goes here. This gets executed after receiving data from the Foursquare API.
+           // $("#results").append(answers);
         });
     
   })});
